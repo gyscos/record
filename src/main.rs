a31319134dd08639e590343f66c3052d30b2b074
Alexandre Bury <alexandre.bury@gmail.com>
2016-10-24 17:15:33 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+#[macro_use]
+extern crate clap;
+extern crate time;
+extern crate pty;
+extern crate pty_shell;
+
+use pty_shell::{PtyShell, PtyCallback};
+
+
+use std::io::Write;
+use clap::{Arg, App};
+
+
+fn main() {
+    let matches = App::new("record")
+        .version(crate_version!())
+        .arg(Arg::with_name("output")
+            .index(1)
+            .required(true)
+            .help("Logs output file"))
+        .get_matches();
+
+    let filename = matches.value_of("output").unwrap();
+
+
+    let mut output = std::fs::File::create(filename).unwrap();
+
+    println!("Starting a recording session. Press Ctrl-D or `exit` to stop.");
+    let child = pty::fork().unwrap();
+
+    let start = time::precise_time_s();
+    child.exec(std::env::var("SHELL").unwrap_or(String::from("bash"))).unwrap();
+    child.proxy(PtyCallback::new()
+        .input(move |input| {
+            write!(&mut output, "{:.3}", time::precise_time_s() - start).unwrap();
+            for &byte in input {
+                write!(&mut output, " {:?}", byte as char).unwrap();
+            }
+            writeln!(&mut output, "").unwrap();
+        })
+        .build()).unwrap();
+
+    child.wait().unwrap();
+}
