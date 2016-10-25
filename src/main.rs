2b2ab5cc1240e0e779d924ae8fee4ce77a5cca9d
Alexandre Bury <alexandre.bury@gmail.com>
2016-10-24 17:36:14 -0700

    Add default output file.

--- src/main.rs
+++ src/main.rs
@@ -13,32 +13,42 @@ use clap::{Arg, App};
 
 fn main() {
     let matches = App::new("record")
+        .about("\
+Starts a recording session.
+All input will be recorded to the given file.
+
+The resulting file will consist in a number of lines, each prefixed with the
+time in seconds since the beginning of the session, and a list of bytes sent.")
         .version(crate_version!())
         .arg(Arg::with_name("output")
             .index(1)
-            .required(true)
-            .help("Logs output file"))
+            .help("Output file. Defaults to `recorded_input`."))
         .get_matches();
 
-    let filename = matches.value_of("output").unwrap();
-
+    let filename = matches.value_of("output").unwrap_or("recorded_input");
 
     let mut output = std::fs::File::create(filename).unwrap();
 
-    println!("Starting a recording session. Press Ctrl-D or `exit` to stop.");
+    println!("\
+Starting a recording session (saving input to `{}`).
+Press Ctrl-D or `exit` to stop.",
+             filename);
     let child = pty::fork().unwrap();
 
     let start = time::precise_time_s();
     child.exec(std::env::var("SHELL").unwrap_or(String::from("bash"))).unwrap();
     child.proxy(PtyCallback::new()
-        .input(move |input| {
-            write!(&mut output, "{:.3}", time::precise_time_s() - start).unwrap();
-            for &byte in input {
-                write!(&mut output, " {:?}", byte as char).unwrap();
-            }
-            writeln!(&mut output, "").unwrap();
-        })
-        .build()).unwrap();
+            .input(move |input| {
+                write!(&mut output, "{:.3}", time::precise_time_s() - start).unwrap();
+                for &byte in input {
+                    write!(&mut output, " {:?}", byte as char).unwrap();
+                }
+                writeln!(&mut output, "").unwrap();
+            })
+            .build())
+        .unwrap();
 
     child.wait().unwrap();
+
+    print!("Input saved as `{}`.\r\n", filename);
 }
