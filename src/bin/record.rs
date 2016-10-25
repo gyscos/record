d051a7e8b0da5f6fbaacc60bd00eb7d0219bc320
Alexandre Bury <alexandre.bury@gmail.com>
2016-10-24 18:54:52 -0700

    Add replay command

--- src/bin/record.rs
+++ src/bin/record.rs
@@ -4,21 +4,25 @@ extern crate time;
 extern crate pty;
 extern crate pty_shell;
 
+use clap::{Arg, App};
 use pty_shell::{PtyShell, PtyCallback};
 
 
 use std::io::Write;
-use clap::{Arg, App};
 
 
 fn main() {
     let matches = App::new("record")
         .about("\
 Starts a recording session.
-All input will be recorded to the given file.
+All input will be recorded to \
+                the given file.
 
-The resulting file will consist in a number of lines, each prefixed with the
-time in seconds since the beginning of the session, and a list of bytes sent.")
+The resulting file will consist in a \
+                number of lines, each prefixed with the
+time in seconds \
+                since the beginning of the session, and a list of bytes \
+                sent.")
         .version(crate_version!())
         .arg(Arg::with_name("output")
             .index(1)
@@ -27,21 +31,24 @@ time in seconds since the beginning of the session, and a list of bytes sent.")
 
     let filename = matches.value_of("output").unwrap_or("recorded_input");
 
-    let mut output = std::fs::File::create(filename).unwrap();
-
     println!("\
 Starting a recording session (saving input to `{}`).
 Press Ctrl-D or `exit` to stop.",
              filename);
-    let child = pty::fork().unwrap();
 
+    let child = pty::fork().unwrap();
+    child.exec(std::env::var("SHELL").unwrap_or(String::from("bash")))
+        .unwrap();
+    // Child stops here. Now it's all the parent.
+
+    let mut output = std::fs::File::create(filename).unwrap();
     let start = time::precise_time_s();
-    child.exec(std::env::var("SHELL").unwrap_or(String::from("bash"))).unwrap();
     child.proxy(PtyCallback::new()
             .input(move |input| {
-                write!(&mut output, "{:.3}", time::precise_time_s() - start).unwrap();
+                write!(&mut output, "{:.3}", time::precise_time_s() - start)
+                    .unwrap();
                 for &byte in input {
-                    write!(&mut output, " {:?}", byte as char).unwrap();
+                    write!(&mut output, "\t{:?}", byte as char).unwrap();
                 }
                 writeln!(&mut output, "").unwrap();
             })
---
+++ src/bin/replay.rs
@@ -0,0 +1,74 @@
+#[macro_use]
+extern crate clap;
+extern crate csv;
+extern crate pty;
+extern crate pty_shell;
+
+use clap::{Arg, App};
+use pty_shell::{PtyShell, PtyCallback};
+use std::io::Write;
+use std::time::Duration;
+
+fn main() {
+    let matches = App::new("replay")
+        .version(crate_version!())
+        .arg(Arg::with_name("input")
+            .index(1)
+            .help("Input file. Defaults to `recorded_input`."))
+        .arg(Arg::with_name("keep")
+            .short("k")
+            .long("keep")
+            .help("Keep the session alive after replay."))
+        .get_matches();
+
+    let filename = matches.value_of("input").unwrap_or("recorded_input");
+    let keep = matches.is_present("keep");
+
+
+    let child = pty::fork().unwrap();
+    child.exec(std::env::var("SHELL").unwrap_or(String::from("bash")))
+        .unwrap();
+    child.proxy(PtyCallback::new().build()).unwrap();
+    // Child stops here. Now it's all the parent.
+
+    println!("Starting replay");
+    let mut reader = csv::Reader::from_file(filename)
+        .unwrap()
+        .has_headers(false)
+        .delimiter(b'\t')
+        .flexible(true);
+    let mut current = 0f64;
+    for record in reader.decode() {
+        let (time, record): (f64, Vec<String>) = record.unwrap();
+        let duration_ms = (1000f64 * (time - current)) as u64;
+        let duration = Duration::from_millis(duration_ms);
+        std::thread::sleep(duration);
+        current = time;
+        for token in record {
+            let token = &token[1..token.len() - 1];
+            let bytes = if token.starts_with('\\') {
+                vec![match token {
+                         "\\r" => b'\r',
+                         "\\t" => b'\t',
+                         other => {
+                             let n = &other[3..other.len() - 1];
+                             u8::from_str_radix(n, 16).unwrap()
+                         }
+                     }]
+            } else {
+                token.as_bytes().to_vec()
+            };
+
+            if !(keep && &bytes == &[4]) {
+                child.pty().unwrap().write_all(&bytes).unwrap();
+            }
+            child.pty().unwrap().flush().unwrap();
+            std::io::stdout().flush().unwrap();
+        }
+    }
+
+
+    child.wait().unwrap();
+
+    print!("Session replay complete.\r\n");
+}
